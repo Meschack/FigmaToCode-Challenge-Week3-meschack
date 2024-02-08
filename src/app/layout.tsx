ab6f0f24c2a70820536d1e91312a4cbe75c09b48
Meschack <meshachgbewezoun@gmail.com>
2024-02-08 11:43:23 +0100

    Change font...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={plusJakartaSans.className}>
         <Header />
         <main>{children}</main>
         <Footer />

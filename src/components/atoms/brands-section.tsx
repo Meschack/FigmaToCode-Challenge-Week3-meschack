b08e446cc2053bc962acc59366b7d1add4e0b6c2
Meschack <meshachgbewezoun@gmail.com>
2024-02-10 17:52:56 +0100

    ...

--- src/components/atoms/brands-section.tsx
+++ src/components/atoms/brands-section.tsx
@@ -4,7 +4,7 @@ export const BrandsSection = () => {
   return (
     <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-16 place-items-center w-full'>
       {Array.from({ length: 6 }).map((_, index) => (
-        <li>
+        <li key={`Brand ${index + 1} logo`}>
           <Image
             src={`/partners/Vector-${index + 1}.svg`}
             alt={`Brand ${index + 1} logo`}
